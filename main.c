2f9c6cf8f8751e0321ef051d7990cb4079dfab88
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-05 16:54:07 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * 
+ * 
+ * 
+ */
+int main(int argc, char *argv[], char *env[])
+{
+    int num;
+    (void)argc;
+
+    /* general commands */
+    char *var[3] = {"exit", "env", "clear"};
+
+    /*            */
+    if (isatty(STDIN_FILENO))
+        write(STDOUT_FILENO, USER, 2);
+
+    num = prompt(argv, env, var);
+    if (num == -1)
+        return (EXIT_FAILURE);
+    return (EXIT_SUCCESS);
+}

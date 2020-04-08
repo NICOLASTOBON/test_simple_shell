588b83cf0b793b11560866a2d072cae98f3c978d
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-07 22:53:15 -0500

    files

--- save_commands.c
+++ save_commands.c
@@ -7,18 +7,24 @@
  */
 char **save_commands(char *buffer)
 {
-    int capacity, use, i, index = 0;
-    char *temp;
-    char **com = malloc(sizeof(char *));
-    if (com == NULL)
-        return (NULL);
+    int i, w, num_words = 0, index = 0;
+    char *temp, **com;
     /* change \n given by getline */
-    buffer[strlen(buffer) -1] = '\0';
+    buffer[strlen(buffer) -1] = '\0';   //
     if (*buffer == '\0')
         return (NULL);
+    /* numbers delimiters */
+    for (w = 0; buffer[w] != '\0' ; w++)
+    {
+        if (buffer[w + 1] == ' ' || buffer[w + 1] == '\0')
+            num_words++;
+    }
+    /* */
+    com = malloc(sizeof(char *) * (num_words + 1));
+    if (com == NULL)
+        return (NULL);
+    /* */
     i = 0;
-    use = 0;
-    capacity = 1;
     while (1)
     {
         temp = own_strtok(buffer, " ", &index);
@@ -31,15 +37,8 @@ char **save_commands(char *buffer)
             return(NULL);
         }
         com[i] = temp;
-        use++;
-        if (use == capacity)
-        {
-            com = realloc(com, sizeof(temp));
-            capacity++;
-        }
         i++;
     }
-    free(temp);
     temp = NULL;
     com[i] = temp;
     //free(temp);
